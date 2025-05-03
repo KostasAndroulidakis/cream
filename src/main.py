8676e5846593ea9baa67411304613f4884a4be0e
KostasAndroulidakis <kostasandroulidakis.pro@gmail.com>
2025-05-03 18:42:04 +0300

    feat: first draft

---
+++ src/main.py
@@ -0,0 +1,39 @@
+from __future__ import annotations
+
+from datetime import datetime
+
+
+class Wallet:
+
+    def __init__(self, *, balance: float = 0.0, history: list | None = None):
+        self.balance = balance
+        self.history = history if history is not None else []
+
+
+class Transcation:
+
+    def __init__(self, *, amount: float, entity: str, datetime: datetime, description: str):
+        self.amount = amount
+        self.entity = entity
+        self.datetime = datetime
+        self.description = description
+
+
+class Incoming(Transcation):
+
+    def __init__(self, *, source: str, **kwargs):
+        super().__init__(entity = source, **kwargs)
+
+    @property
+    def source(self) -> str:
+        return self.entity
+
+
+class Outgoing(Transcation):
+
+    def __init__(self, *, target: str, **kwargs):
+        super().__init__(entity = target, **kwargs)
+
+    @property
+    def source(self) -> str:
+        return self.entity
