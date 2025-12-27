61cbab012ba412644454099ce8613d08427b7bc5
KostasAndroulidakis <kostasandroulidakis.pro@gmail.com>
2025-12-27 15:16:06 +0200

    Set up C++ project structure

---
+++ core/include/cream/core.hpp
@@ -0,0 +1,16 @@
+#ifndef CREAM_CORE_HPP
+#define CREAM_CORE_HPP
+
+// CREAM C++ Core Library
+// Finance engine for ledger, validation, and calculations
+
+namespace cream {
+
+// Version info
+constexpr int VERSION_MAJOR = 0;
+constexpr int VERSION_MINOR = 1;
+constexpr int VERSION_PATCH = 0;
+
+} // namespace cream
+
+#endif // CREAM_CORE_HPP
